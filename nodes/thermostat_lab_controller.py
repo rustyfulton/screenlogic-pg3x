f21dbfaa7b7a60ab217144b7aa27b62f956c583d
rfulton <rustyfulton@gmail.com>
2026-05-22 07:20:58 -0700

    Clean up stale thermostat lab nodes

--- nodes/thermostat_lab_controller.py
+++ nodes/thermostat_lab_controller.py
@@ -15,6 +15,8 @@ class ThermostatLabControllerNode(ExperimentalPoolTempDocCloneHintThermostatNode
 
     def start(self):
         LOGGER.info("Starting thermostat lab controller node as root thermostat")
+        self._cleanup_stale_lab_nodes()
+        self._ensure_name("Thermostat Lab")
         if self.alarm_node is None:
             self.alarm_node = ExperimentalPoolTempPowerAlarmNode(
                 self.poly,
@@ -25,6 +27,46 @@ class ThermostatLabControllerNode(ExperimentalPoolTempDocCloneHintThermostatNode
             )
             self.poly.addNode(self.alarm_node)
         self.refresh_children(refresh_topology=True)
+
+    def _cleanup_stale_lab_nodes(self):
+        get_node = getattr(self.poly, "getNode", None)
+        del_node = getattr(self.poly, "delNode", None)
+        if not callable(get_node) or not callable(del_node):
+            return
+        for address in ("labtstat", "labtstata"):
+            node = get_node(address)
+            if node is None:
+                continue
+            LOGGER.info(
+                "Thermostat lab controller removing stale prior lab node address=%s",
+                address,
+            )
+            try:
+                del_node(address)
+            except Exception:
+                LOGGER.exception(
+                    "Unable to remove stale thermostat lab node address=%s",
+                    address,
+                )
+
+    def _ensure_name(self, desired_name):
+        if getattr(self, "name", None) == desired_name:
+            return
+        LOGGER.info(
+            "Thermostat lab controller renaming root node old=%s new=%s",
+            getattr(self, "name", "<unknown>"),
+            desired_name,
+        )
+        self.name = desired_name
+        rename = getattr(self, "setName", None)
+        if callable(rename):
+            try:
+                rename(desired_name)
+            except Exception:
+                LOGGER.exception(
+                    "Unable to rename thermostat lab root node to %s",
+                    desired_name,
+                )
 
     def set_client(self, client, **_kwargs):
         self.client = client
@@ -65,4 +107,3 @@ class ThermostatLabControllerNode(ExperimentalPoolTempDocCloneHintThermostatNode
     def query(self, command=None):
         LOGGER.info("Thermostat lab query invoked command=%s", command)
         self.refresh_children(refresh_topology=True)
-
